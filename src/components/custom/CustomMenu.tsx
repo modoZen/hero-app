733e40c9708fa29080074627aa8a744a01f430d2
Max Diaz <mdiazn@uni.pe>
2025-10-22 12:37:34 -0500

    add CustomMenu component in HeroesLayout

---
+++ src/components/custom/CustomMenu.tsx
@@ -0,0 +1,43 @@
+import { cn } from "@/lib/utils";
+import {
+  NavigationMenuItem,
+  NavigationMenuLink,
+} from "@radix-ui/react-navigation-menu";
+import { Link, useLocation } from "react-router";
+import { NavigationMenu, NavigationMenuList } from "../ui/navigation-menu";
+
+export const CustomMenu = () => {
+  const { pathname } = useLocation();
+
+  const isActive = (path: string) => {
+    return pathname === path;
+  };
+
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        {/*Home*/}
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            asChild
+            className={cn(isActive("/") && "bg-slate-200 ", "rounded-md p-2")}
+          >
+            <Link to="/">Inicio</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        {/*Search*/}
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            asChild
+            className={cn(
+              isActive("/search") && "bg-slate-200 ",
+              "rounded-md p-2"
+            )}
+          >
+            <Link to="/search">Buscar superhéroes</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
